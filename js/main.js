10e80077146e00e1fac56d194f76a133d9478540
Bruce Garro <brucegarro@gmail.com>
2018-01-07 18:25:31 -0500

    Replaces one word

--- js/main.js
+++ js/main.js
@@ -1,2 +1,28 @@
-console.log("LOG: Hello World!");
-alert("Alert: Hello World!");
+'use strict';
+
+var wrapTextInSpan = function(targetText) {
+	var targetDivs = "p";
+	var html = $(targetDivs).html();
+
+	// if (!html) {
+	// 	html = $(targetDivs).text();
+	// }
+
+	$(targetDivs).html(
+		html.replace(targetText, `<span style="color: red">${targetText}</span>`)
+	);
+	// targetDivs.contents().filter(function(){
+	//     return this.nodeType == 3 && $.trim(this.nodeValue).length;
+	// }).wrap('<span style="color:red;"/>');
+}
+
+var main = function() {
+	var targetText = "计算";
+	console.log("Begin Main");
+	wrapTextInSpan(targetText);
+	console.log("End Main");
+};
+
+$(function() {
+	main();
+});
